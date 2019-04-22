ab41e5b3afe40224e59ce851aa93899809ba8a35
JoeyMcNatt <36242721+JoeyMcNatt@users.noreply.github.com>
2019-04-21 21:02:31 -0400

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -48,6 +48,8 @@ volume_4 = Dropout(.25)(volume_4)
 # Concatenate all volumes of the Inception module
 inception_module = keras.layers.concatenate([volume_1, volume_2, volume_3,
                                              volume_4], axis = 3)
+inception_module = Conv2D(128, (5,5), padding='same', activation='relu')(inception_module)
+inception_module = Dropout(.25)(inception_module)
 inception_module = Conv2D(64, (3,3), padding='same', activation='relu')(inception_module)
 output = Flatten()(inception_module)
 out    = Dense(10, activation='softmax')(output)
